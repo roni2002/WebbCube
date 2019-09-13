937cad95b6424341ba1ec1c01e89ff6763e41917
AlphaInterWoof (Tee.A.J) <18059182+AlphaInterWoof@users.noreply.github.com>
2019-09-13 10:47:49 +0200

    WebbCube

---
+++ Assets/Cube.cs
@@ -0,0 +1,41 @@
+﻿// Förslagsvis lägg detta som ett nytt skript på en 3D cube objekt
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    void Start()
+    {
+        //StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        while (true)
+        {
+            Debug.Log("Time: " + Time.time);
+            UpdateHTMLDOM();
+            yield return new WaitForSeconds(1f);
+
+        }
+
+    }
+
+    void UpdateHTMLDOM()
+    {
+        Debug.Log("Time: " + Time.time);
+        // Anropa HTML5 DOM komponenten genom att använda
+        // https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
